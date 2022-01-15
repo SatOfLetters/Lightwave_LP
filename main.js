7c41a200b1937b37069f9b98c005064418d34802
SatOfLetters <97686395+SatOfLetters@users.noreply.github.com>
2022-01-15 19:51:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+ScrollReveal().reveal('.scroll_reveal1', {
+    duration: 2500
+});
+
+ScrollReveal().reveal('.scroll_reveal2', {
+    duration: 2500,
+    delay: 300
+});
+
+ScrollReveal().reveal('.scroll_reveal3', {
+    duration: 2500,
+    delay: 900
+});
+
+ScrollReveal().reveal('.scroll_reveal4', {
+    duration: 2500,
+    delay: 1200
+});
